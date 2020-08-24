3697cbadbf52a38c8925d19bd7f7d7d675920ded
Tamires Santos <tfs.tamsantos@gmail.com>
2020-08-24 14:35:45 -0300

    Add backend and frontend

---
+++ backend/src/index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4, validate } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectsId(request, response, next) {
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Invalid project ID." });
+  }
+
+  return next();
+}
+
+// app.use(logRequests);
+app.use("/projects/:id", validateProjectsId);
+
+app.get("/projects", logRequests, (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
+});
+
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuidv4(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Backend started");
+});
